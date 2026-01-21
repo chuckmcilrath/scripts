3680d4e483a9a214f933db18c3b55887c5690ade
chuckmcilrath <chuckmcilrath@gmail.com>
2026-01-21 12:01:12 -0800

    Update ChangeWGName.sh

--- ChangeWGName.sh
+++ ChangeWGName.sh
@@ -9,6 +9,7 @@ wg2_private="/etc/wireguard/wg1_private.key"
 wg1_public="/etc/wireguard/wg0_public.key"
 wg2_public="/etc/wireguard/wg1_public.key"
 
+dcm_conf="etc/wireguard/dcm.conf"
 dcm_private="etc/wireguard/dcm_private.key"
 dcm_public="etc/wireguard/dcm_public.key"
 
@@ -17,14 +18,14 @@ wan_peer_change="dcm.genteks.net"
 if grep -q "AllowedIPs = 10.100.100.0/24" "$wg_config1"; then
 	cp "$wg_config1" "$wg_config1".bak
 	sed -i -E "s/(Endpoint = )([^:]+)(:[0-9]+)/\1$wan_peer_change\3/" "$wg_config1"
-	mv "$wg_config1" /etc/wireguard/dcm.conf
+	mv "$wg_config1" "$dcm_conf"
 	mv "$wg1_private" "$dcm_private"
 	mv "$wg1_public" "$dcm_public"
 	sed -i 's/wg0/dcm/' ~/.bashrc
 elif grep -q "AllowedIPs = 10.100.100.0/24" "$wg_config2"; then
 	cp "$wg_config2" "$wg_config2".bak
 	sed -i -E "s/(Endpoint = )([^:]+)(:[0-9]+)/\1$wan_peer_change\3/" "$wg_config2"
-	mv "$wg_config2" /etc/wireguard/dcm.conf
+	mv "$wg_config2" "$dcm_conf"
 	mv "$wg2_private" "$dcm_private"
 	mv "$wg2_public" "$dcm_public"
 	sed -i 's/wg1/dcm/' ~/.bashrc
