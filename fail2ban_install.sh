9664a01e0f46a4c8b5e09a28de6dd29c71e92ffe
chuckmcilrath <chuckmcilrath@gmail.com>
2026-05-27 15:34:56 -0700

    Update fail2ban_install.sh

--- fail2ban_install.sh
+++ fail2ban_install.sh
@@ -5,65 +5,121 @@
 #
 #
 
-if ! which fail2ban-client &> /dev/null
-    then
-        echo "fail2ban not found. Installing..."
-        apt update && apt install fail2ban -y
-        if which fail2ban-client &> /dev/null
-            then
+#VARIABLES
+defaults_jail_path=(/etc/fail2ban/jail.d/defaults.local)
+ssh_jail_path=(/etc/fail2ban/jail.d/sshd.local)
+proxmox_jail_path=(/etc/fail2ban/jail.d/proxmox.local)
+proxmox_filter_path=/etc/fail2ban/filter.d/proxmox.conf
+f2b_local_path=/etc/fail2ban/fail2ban.local
+
+filter_def="[Definition]\nfailregex = pvedaemon\[.*authentication failure; rhost=<HOST> user=.* msg=.*\nignoreregex ="
+
+#FUNCTIONS
+check_root() {
+	if [[ $EUID -ne 0 ]]; then
+		echo "This script must be run as root. Please use sudo or log in as root." >&2
+		exit 1
+	fi
+}
+
+check_fail2ban_client_install() {
+    if ! which fail2ban-client &> /dev/null; then
+            echo "fail2ban not found. Installing..."
+            apt update && apt install fail2ban -y
+        if which fail2ban-client &> /dev/null; then
                 echo "fail2ban has been successfully installed."
             else
                 echo "Installation failed."
-                exit 1 #stop the script if installtion fails.
+                exit 1
+        fi
+    else
+        echo "fail2ban is already installed."
     fi
-else
-    echo "fail2ban is already installed."
-fi
+}
 
-jail_conf=/etc/fail2ban/jail.conf
-jail_local=/etc/fail2ban/jail.local
+# User Input
+user_input() {
+    read -rp "Enter an email for fail2ban to use as a \"sender\". (e.g. fail2ban@name-of-company.com)" sender_email
+    read -rp "Enter the amount of time to ban offenders? (e.g. 5m, 12h, 50d, 10y etc)." bantime_input
+    read -rp "Enter the maximum amount of retries before banning." maxretry_input
+}
 
-#Copy the jail.conf into a .local, which is to be taken by fail2ban as the real config.
-cp $jail_conf $jail_local
+defaults() {
+		cat <<EOF > "$defaults_jail_path"
+[DEFAULT]
+bantime = "$bantime_input"
+bantime = "$maxretry_input"
+findtime = 10m
+backend = systemd
+destemail = root@localhost
+sender = "$sender_email"
+mta = sendmail
+action = %(action_mwl)s
+ignoreip = 127.0.0.1/8 ::1
+EOF
+}
 
-#Input from user.
-echo "Enter an email for fail2ban to use as a \"sender\". (e.g. fail2ban@name-of-company.com)"
-read sender_email
+ssh_jail() {
+		cat <<EOF > "$ssh_jail_path"
+[sshd]
+enabled = true
+port = ssh
+filter = sshd
+EOF
+}
 
-echo "Enter the amount of time to ban offenders? (e.g. 5m, 12h, 50d, 10y etc)."
-read bantime_input
+proxmox_jail() {
+		cat <<EOF > "$proxmox_jail_path"
+[proxmox]
+enabled = true
+port = https,http,8006
+filter = proxmox
+EOF
+}
 
-echo "Enter the maximum amount of retries before banning."
-read maxretry_input
+Proxmox_filter() {
+    if [ ! -f "$proxmox_filter_path" ]; then
+        touch $proxmox_filter_path
+        echo -e "$filter_def" > $proxmox_filter_path
+    fi
+}
 
+fail2ban_local_add_for_IPV6() {
+    if [ ! -f "$f2b_local_path" ]; then
+        touch $f2b_local_path
+    fi
 
-#Editing the .local file.
-sed -i -e "s/sender = root@<fq-hostname>/sender = $sender_email/" \
-       -e 's/^action = %(action_)s/action = %(action_mw)s/' \
-       -e 's/backend = auto/backend = systemd/' \
-       -e "s/bantime  = 10m/bantime = $bantime_input/" \
-       -e "s/maxretry = 5/maxretry = $maxretry_input/" \
-       -e '/^\[sshd\]/a\enabled = true' "$jail_local"
+    cat <<EOF > "$f2b_local_path"
+[DEFAULT]
+allowipv6 = no
+loglevel = INFO
+logtarget = /var/log/fail2ban.log
+syslogsocket = auto
+socket = /var/run/fail2ban/fail2ban.sock
+pidfile = /var/run/fail2ban/fail2ban.pid
+dbfile = /var/lib/fail2ban/fail2ban.sqlite3
+dbpurgeage = 1d
+dbmaxmatches = 10
+[Definition]
+[Thread]
+EOF
+}
 
-echo -e "\n[proxmox]\nenabled = true \
-\nport = https,http,8006 \
-\nfilter = proxmox" >> "$jail_local"
+finish() {
+    systemctl restart fail2ban
+    echo -e "Fail2ban has been successfully started\nTo see status of the server, please type: \"systemctl status fail2ban\""
+    echo -e "USEFUL COMMANDS\nfail2ban-client status proxmox\nfail2ban-client unban ip (IP ADDRESS)"
+}
 
-#Creating and editing the proxmox filter.
-proxmox_filter=/etc/fail2ban/filter.d/proxmox.conf
-filter_def="[Definition]\nfailregex = pvedaemon\[.*authentication failure; rhost=<HOST> user=.* msg=.*\nignoreregex ="
-touch $proxmox_filter
+# MAIN
+check_root
+check_fail2ban_client_install
+user_input
+defaults
+ssh_jail
+proxmox_jail
+Proxmox_filter
+fail2ban_local_add_for_IPV6
+finish
 
-echo -e "$filter_def" > $proxmox_filter
-
-#This sets up fail2ban to not allow ipv6 connections. Mainly here to satisfy a pesky error.
-f2b_conf=/etc/fail2ban/fail2ban.conf
-f2b_local=/etc/fail2ban/fail2ban.local
-cp $f2b_conf $f2b_local
-sed -i '/^\[DEFAULT\]/a\allowipv6 = no' $f2b_local
-
-#restart fail2ban to apply changes.
-systemctl restart fail2ban
-
-echo -e "Fail2ban has been successfully started\nTo see status of the server, please type: \"systemctl status fail2ban\""
-echo -e "USEFUL COMMANDS\nfail2ban-client status proxmox\nfail2ban-client unban ip (IP ADDRESS)"
+## END OF SCRIPT
